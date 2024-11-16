b510a653a5d5595a0d0d8a06416a248bbdb5c24c
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2024-11-16 15:20:03 -0300

    alocaçãodememoria

---
+++ Códigos de estudo/alocaçãodpv.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int *alocaVetor(int a)
+{
+    int *aux;
+    aux = (int *)malloc(a * sizeof(int));
+    if(aux==NULL){
+        printf("Erro na alocacao de memoria!");
+        exit(1);
+    }
+    return aux;
+}
+int main()
+{
+    int a, contador, *ponteiro;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &a);
+    ponteiro = alocaVetor(a);
+    for (contador = 0; contador < a; contador++)
+    {
+        printf("Digite o valor de vetor[%d]: ", contador);
+        scanf("%d", &ponteiro[contador]);
+    }
+    for (contador = 0; contador < a; contador++)
+    {
+        printf("vetor[%d]\n", ponteiro[contador]);
+    }
+    free(ponteiro);
+    return 0;
+}
