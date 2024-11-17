d9d648638db972984f80be806a7e6301a0ef4aae
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2024-11-17 19:29:07 -0300

    alocaçãodememoriac++

--- Códigos de estudo/alocaçãodpv.cpp
+++ Códigos de estudo/alocaçãodpv.cpp
@@ -2,22 +2,31 @@
 #include <stdlib.h>
 #include <new>
 
-int main(){
+int main()
+{
     int contador, a, *ponteiro;
     printf("Por favor, digite o tamanho do vetor: ");
     scanf("%d", &a);
 
     ponteiro = new int[a];
+    if (ponteiro == NULL)
+    {
+        printf("Erro na alocacao de memoria!");
+        exit(1);
+    }
 
-    for(contador=0;contador<a;contador++){
+    for (contador = 0; contador < a; contador++)
+    {
         printf("Digite o valor de vetor[%d]: ", contador);
         scanf("%d", &ponteiro[contador]);
     }
 
-    for(contador=0;contador<a;contador++){
-        printf("vetor[%d] = %d\n", contador,ponteiro[contador]);
+    for (contador = 0; contador < a; contador++)
+    {
+        printf("vetor[%d] = %d\n", contador, ponteiro[contador]);
     }
 
+    free(ponteiro);
 
     return 0;
 }
