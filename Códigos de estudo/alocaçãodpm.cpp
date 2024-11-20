0fcdea19c682f4426713c8a7022a244f220a5af0
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2024-11-20 13:53:40 -0300

    alocaçãodememoriaM++

---
+++ Códigos de estudo/alocaçãodpm.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <new>
+
+int **alocaMatriz(int a, int b)
+{
+    int **aux = new int*[a];
+    
+    for (int i = 0; i < a; i++)
+    {
+        aux[i] = new int[b];
+        
+    }
+    return aux;
+}
+
+int main()
+{
+    int a, b, contador, contador1, **ponteiro;
+    printf("Quantidade de linhas: ");
+    scanf("%d", &a);
+    printf("Quantidade de colunas: ");
+    scanf("%d", &b);
+
+    if (a <= 0 || b <= 0)
+    {
+        printf("Linhas e colunas devem ser positivos!");
+        return (1);
+    }
+
+    ponteiro = alocaMatriz(a, b);
+
+    for (contador = 0; contador < a; contador++)
+    {
+        for (contador1 = 0; contador1 < b; contador1++)
+        {
+            printf("Digite matriz[%d][%d]: ", contador, contador1);
+            scanf("%d", &ponteiro[contador][contador1]);
+        }
+    }
+    for (contador = 0; contador < a; contador++)
+    {
+        for (contador1 = 0; contador1 < b; contador1++)
+        {
+            printf("matriz[%d][%d] = %d\n", contador, contador1, ponteiro[contador][contador1]);
+        }
+    }
+
+    for (contador = 0; contador < a; contador++)
+    {
+        delete(ponteiro[contador]);
+    }
+
+    delete(ponteiro);
+
+    return 0;
+}
